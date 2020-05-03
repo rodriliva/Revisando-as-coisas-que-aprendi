afd992dfd50542f13745d3849513bcbf083ffa23
rodrigoliva <rodrigolivavelho@gmail.com>
2020-05-03 01:53:20 -0300

    Atualização Final

--- Arquivos para estudo/javascript.js
+++ Arquivos para estudo/javascript.js
@@ -1,28 +1,8 @@
-let Rodrigo = {
-    nome: "Rodrigo",
-    categoria: "Mitos",
-}
-let Giovani = {
-    nome: "Giovanni",
-    categoria: "Mulheres",
-}
-let MariBaianinha = {
-    nome: "Mari Baianinha",
-    categoria: "Mulheres",
-}
-let Bolsonaro = {
-    nome: "Bolsonaro",
-    categoria: "Mitos",
+txt = ""
+var person = {fname:"John", lname:"Doe", age:25};
+
+for (x in person) {
+  txt += person[x]+ " ";
 }
 
-const pessoas = [Rodrigo, Giovani, MariBaianinha, Bolsonaro]
-
-
-text = ""
-
-while (i < 10) {
-    text += "The number is " + i;
-    i++;
-  }
-
-  console.log(text)
+console.log(txt)
--- Javascript.md
+++ Javascript.md
@@ -106,6 +106,19 @@ var person = {
   }
 };
 ```
+
+**Objetos Construtores**
+```
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+
+var myFather = new Person("John", "Doe", 50, "blue");
+```
+
 ## Arrays
 **Array simples**
 
